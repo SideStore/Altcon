91771d0abaa0d4846fa81a6ea413f9d7aaa2ad9f
Macleykun <wesley.de.vree@gmail.com>
2023-02-11 12:07:35 +0100

    Full build incomming :)

--- downloader.sh
+++ downloader.sh
@@ -2,7 +2,7 @@
 
 # Check if we're running 32-bit arm, x86 or else aarch64/x86_64
 if [[ "$(uname -m)" = 'arm'* ]]; then
-  curl -s https://api.github.com/repos/NyaMisty/AltServer-Linux/releases/latest | grep "browser_download_url.*arm*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+  curl -s https://api.github.com/repos/NyaMisty/AltServer-Linux/releases/latest | grep "browser_download_url.*armv*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 else
   curl -s https://api.github.com/repos/NyaMisty/AltServer-Linux/releases/latest | grep "browser_download_url.*$(uname -m)*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 fi

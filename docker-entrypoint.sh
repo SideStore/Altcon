e87db4d793275086d78c6c806a010889a89fd712
Macleykun <wesley.de.vree@gmail.com>
2023-02-11 14:23:41 +0100

    refactor to not use wget

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,8 +8,8 @@ done
 
 udid=$(idevice_id | awk '{print $1}')
 echo -e "\nTo install an IPA, run the following command and change \033[0;31mmyemail\033[0m and \033[0;31mmyapplepass\033[0m. The \033[0;32mUDID\033[0m is already correct:"
-echo -e "./AltServer -u \033[0;32m${udid}\033[0m -a \033[0;31mmyemail@mail.com\033[0m -p \033[0;31mmyapplepass\033[0m SideStore.ipa"
-echo =============================================================================================================
+echo -e "./AltServer -u \033[0;32m${udid}\033[0m -a \033[0;31mmyemail@mail.com\033[0m -p \033[0;31mmyapplepass\033[0m SideStore.ipa\n"
+printf =============================================================================================================
 printf "Once you're finished, type exit to exit the container you're now in.\n\n"
 
 /bin/bash
---
+++ get-altserver-linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check if we're running 32-bit arm, x86 or else aarch64/x86_64
+if [[ "$(uname -m)" = 'arm'* ]]; then
+  curl -sL -o AltServer $(curl -s https://api.github.com/repos/NyaMisty/AltServer-Linux/releases/latest | grep "browser_download_url.*armv*" | cut -d : -f 2,3 | tr -d \")
+else
+  curl -sL -o AltServer $(curl -s https://api.github.com/repos/NyaMisty/AltServer-Linux/releases/latest | grep "browser_download_url.*$(uname -m)*" | cut -d : -f 2,3 | tr -d \")
+fi
+chmod +x AltServer

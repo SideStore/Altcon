fba7b6b549f781748069b267bcf876db94ad24ee
Macley <26381427+Macleykun@users.noreply.github.com>
2023-02-18 11:35:02 +0100

    Have altserver and sidestore in image

--- get-altserver-linux.sh
+++ get-altserver-linux.sh
@@ -7,3 +7,7 @@ else
   curl -sL -o AltServer $(curl -s https://api.github.com/repos/NyaMisty/AltServer-Linux/releases/latest | grep "browser_download_url.*$(uname -m)*" | cut -d : -f 2,3 | tr -d \")
 fi
 chmod +x AltServer
+
+# Get SideStore Stable and Nightly
+curl -sL -o SideStore.ipa $(curl -s https://github.com/SideStore/SideStore/releases/latest/download/SideStore.ipa | grep "browser_download_url.*armv*" | cut -d : -f 2,3 | tr -d \")
+curl -sL -o SideStore-Nightly.ipa $(curl -s https://github.com/SideStore/SideStore/releases/download/nightly/SideStore.ipa | grep "browser_download_url.*armv*" | cut -d : -f 2,3 | tr -d \")

95dbcc1bd88270c68b9f49672d6494d21f0eebc0
Macley <26381427+Macleykun@users.noreply.github.com>
2023-02-18 11:35:52 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# See if we can get the latest, it's ok if these fail, they are in the image still
+# Check if we're running 32-bit arm, x86 or else aarch64/x86_64
+if [[ "$(uname -m)" = 'arm'* ]]; then
+  curl -sL -o AltServer $(curl -s https://api.github.com/repos/NyaMisty/AltServer-Linux/releases/latest | grep "browser_download_url.*armv*" | cut -d : -f 2,3 | tr -d \")
+else
+  curl -sL -o AltServer $(curl -s https://api.github.com/repos/NyaMisty/AltServer-Linux/releases/latest | grep "browser_download_url.*$(uname -m)*" | cut -d : -f 2,3 | tr -d \")
+fi
+chmod +x AltServer
+
+# Get SideStore Stable and Nightly
+curl -sL -o SideStore.ipa $(curl -s https://github.com/SideStore/SideStore/releases/latest/download/SideStore.ipa | grep "browser_download_url.*armv*" | cut -d : -f 2,3 | tr -d \")
+curl -sL -o SideStore-Nightly.ipa $(curl -s https://github.com/SideStore/SideStore/releases/download/nightly/SideStore.ipa | grep "browser_download_url.*armv*" | cut -d : -f 2,3 | tr -d \")
+
 printf "\nPlug in your idevice and enter your passcode.\n"
 printf "=============================================\n"
 while ! idevicepair pair; do

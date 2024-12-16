4fbfeb098a4c0f0357d8d35a970d4e1c8e3c3eb4
Macley <26381427+Macleykun@users.noreply.github.com>
2024-12-16 15:05:09 +0100

    Explain issue with complex/random passwords

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -27,7 +27,12 @@ printf "===============================================================\n"
 echo -e "\nTo install an IPA, run the following command and change \033[0;31mmyemail\033[0m and \033[0;31mmyapplepass\033[0m. The \033[0;32mUDID\033[0m is already correct:"
 printf "=============================================================================================================\n"
 echo -e "./AltServer -u \033[0;32m${udid}\033[0m -a \033[0;31mmyemail@mail.com\033[0m -p \033[0;31mmyapplepass\033[0m SideStore.ipa\n"
-echo -e "Once you're finished, type: \033[0;35mexit\033[0m."
+echo -e "\nDo note that if your password contains special characters like '@','$' '!'or a space."
+echo "It may not work and you need to reset your password to something simpler sadly."
+echo "You can type in: 'sh' and then the command above which may or may not work for your password."
+echo "But using arrow keys and tab completion won't work, so only copy, paste and enter works."
+printf "=============================================================================================\n"
+echo -e "\nOnce you're finished, type: \033[0;35mexit\033[0m."
 printf "=================================\n\n"
 
-/bin/sh
+/bin/bash
